3d2912887689e6d935be53a8afa1c6a2e593178d
Royalvenkat <venkatsai3099@gmail.com>
2024-12-19 16:06:58 +0530

    Again Pull

--- src/App.js
+++ src/App.js
@@ -3,12 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-      <h1>Venkat sai royal</h1>
-=======
-      <h1>Venkat sai in downloads 2
-      </h1>
->>>>>>> ad93530b49b02c7ea8129ad7ca64255d7c22c2fc
+      <h1>Venkat sai in downloads 2</h1>
       <form>
         <input type="text" />
         </form>
