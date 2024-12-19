c1b4ddfbc2394bca575876b2deaf04103f7dbfa8
Royalvenkat <venkatsai3099@gmail.com>
2024-12-19 16:03:20 +0530

    done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ const App = () => {
 <<<<<<< HEAD
       <h1>Venkat sai royal</h1>
 =======
-      <h1>Venkat sai in downloads</h1>
+      <h1>Venkat sai in downloads 2
+      </h1>
 >>>>>>> ad93530b49b02c7ea8129ad7ca64255d7c22c2fc
       <form>
         <input type="text" />
